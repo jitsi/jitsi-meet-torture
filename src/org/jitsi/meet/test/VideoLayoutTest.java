57a1fbbad72b5f829269cf0d7b6cb580ecdcb093
yanas <yana@jitsi.org>
2015-10-15 11:28:30 -0500

    Adds a video layout test.

---
+++ src/org/jitsi/meet/test/VideoLayoutTest.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright @ 2015 Atlassian Pty Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jitsi.meet.test;
+import junit.framework.*;
+
+import org.openqa.selenium.*;
+
+/**
+ * Tests the video layout by checking its width and height compared to the
+ * window width and height. This is meant to prove that video is correctly
+ * aligned and sized at the beginning of the call.
+ *
+ * TODO: We may add additional checks asserting video is the correct size,
+ * when the chat is opened or after a desktop sharing session.
+ *
+ * @author Yana Stamcheva
+ */
+public class VideoLayoutTest
+    extends TestCase
+{
+    /**
+     * Tests the video layout. This is meant to prove that video is correctly
+     * aligned and sized at the beginning of the call.
+     *
+     * TODO: Add tests which turn on/off screen sharing and then check if
+     * the video would fit back to the screen.
+     */
+    public void testVideoLayout()
+    {
+        System.err.println("Start testVideoLayout.");
+
+        WebDriver owner = ConferenceFixture.getOwner();
+
+        String chatXPath = "//div[@id='chatspace']";
+        String contactListXPath = "//div[@id='contactlist']";
+        String settingsXPath = "//div[@id='settingsmenu']";
+
+        WebElement chatElem = owner.findElement(By.xpath(chatXPath));
+        WebElement contactListElem
+                = owner.findElement(By.xpath(contactListXPath));
+        WebElement settingsElem = owner.findElement(By.xpath(settingsXPath));
+
+        if (!chatElem.isDisplayed()
+                && !contactListElem.isDisplayed()
+                && !settingsElem.isDisplayed())
+        {
+            doLargeVideoSizeCheck(owner);
+        }
+    }
+
+    /**
+     * Checks if the video container fits the inner window width and height.
+     *
+     * @param webDriver <tt>WebDriver</tt> instance of the participant for whom
+     *                  we'll try to check the video size
+     */
+    private void doLargeVideoSizeCheck(WebDriver webDriver)
+    {
+        Long innerWidth = (Long)((JavascriptExecutor) webDriver)
+                .executeScript("return window.innerWidth;");
+
+        Long innerHeight = (Long)((JavascriptExecutor) webDriver)
+                .executeScript("return window.innerHeight;");
+
+        WebElement largeVideo = webDriver.findElement(
+                By.xpath("//div[@id='largeVideoContainer']"));
+
+        assertEquals(largeVideo.getSize().getWidth(), innerWidth.intValue());
+        assertEquals(largeVideo.getSize().getHeight(), innerHeight.intValue());
+    }
+}
