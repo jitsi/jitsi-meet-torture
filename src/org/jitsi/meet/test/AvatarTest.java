c1c3106cef13418747a4593f06106fcab45c8d42
damencho <damencho@jitsi.org>
2016-03-17 23:37:13 -0500

    Fixes wrong timeout value.

--- src/org/jitsi/meet/test/AvatarTest.java
+++ src/org/jitsi/meet/test/AvatarTest.java
@@ -219,7 +219,7 @@ public class AvatarTest
 
         // Wait for the avatar element to be created
         TestUtils.waitForElementByXPath(
-            secondParticipant, ownerAvatarXPath, 5000);
+            secondParticipant, ownerAvatarXPath, 5);
 
         final String srcOneSecondParticipant =
             getSrcByXPath(secondParticipant, ownerAvatarXPath);
