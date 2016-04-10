0ace5f0298d5dfa8e67e379f29aa6b90a7a82712
hristoterezov <hristo@jitsi.org>
2016-04-10 12:22:59 -0500

    Implements performance time tests

---
+++ src/org/jitsi/meet/test/PerformanceTimeTest.java
@@ -0,0 +1,388 @@
+/*
+ * Copyright @ 2015 Atlassian Pty Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jitsi.meet.test;
+
+import java.util.Arrays;
+
+import org.jitsi.meet.test.util.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.*;
+
+import junit.framework.*;
+
+/**
+ * This test is going to get the performance times measurements from Jitsi Meet
+ * and fail if they are too slow.
+ * 
+ *
+ * @author Hristo Terezov
+ */
+public class PerformanceTimeTest
+    extends TestCase
+{
+    /**
+     * Number of conferences that are going to be started and closed to 
+     * gather the data.
+     */
+    private static int NUMBER_OF_CONFERENCES = 10;
+    
+    /**
+     * The second participant. We are using that property to refresh the second
+     * tab.
+     */
+    private static WebDriver participant 
+        = ConferenceFixture.getSecondParticipant();
+    
+    /**
+     * Enum that represents the types of time measurements. We are storing the
+     * scripts that is used to get the value for that type, a previous step and
+     * threshold.
+     */
+    private static enum TimeMeasurements
+    {
+        INDEX_LOADED("return APP.performanceTimes['index.loaded']", null, 200.0),
+        
+        DOCUMENT_READY("return APP.performanceTimes['document.ready']", 
+            INDEX_LOADED, 600.0),
+        
+        CONNECTION_ATTACHING(
+            "return APP.connection.getPerformanceTimes()['attaching']",
+            DOCUMENT_READY, 350.0),
+        
+        CONNECTION_ATTACHED(
+            "return APP.connection.getPerformanceTimes()['attached']", 
+            CONNECTION_ATTACHING, 5.0),
+        
+        CONNECTION_CONNECTING(
+            "return APP.connection.getPerformanceTimes()['connecting']", 
+            DOCUMENT_READY, 350.0),
+        
+        CONNECTION_CONNECTED(
+            "return APP.connection.getPerformanceTimes()['connected']", 
+            CONNECTION_CONNECTING, 1000.0),
+        
+        MUC_JOINED(
+            "return APP.conference._room.getPerformanceTimes()['muc.joined']",
+            null, 500.0),
+        
+        SESSION_INITIATE("return APP.conference._room.getPerformanceTimes()"
+            + "['session.initiate']", MUC_JOINED, 600.0),
+        
+        ICE_CHECKING("return APP.conference._room.getPerformanceTimes()"
+            + "['ice.state.checking']", SESSION_INITIATE, 150.0),
+        
+        ICE_CONNECTED("return APP.conference._room.getPerformanceTimes()"
+            + "['ice.state.connected']", ICE_CHECKING, 500.0),
+        
+        AUDIO_RENDER("return APP.performanceTimes['audio.render']", 
+            ICE_CONNECTED, 200.0),
+        
+        VIDEO_RENDER("return APP.performanceTimes['video.render']", 
+            ICE_CONNECTED, 200.0);
+        
+        /**
+         * The script used to get the data for a time measurement type.
+         */
+        private String script;
+        
+        /**
+         * Max time between the previous measurement and 
+         * the current one
+         */
+        private Double threshold;
+        
+        /**
+         * The previous executed time measurement. We are going to compare the 
+         * period of time between 2 consecutive time measurements and the 
+         * threshold.
+         */
+        private TimeMeasurements prevStep;
+        
+        /**
+         * The number of time measurements/
+         */
+        public static final int length = TimeMeasurements.values().length;
+        
+        /** 
+         * Construct new TimeMeasurements instance.
+         * @param script The script used to get the data for a time 
+         * measurement type.
+         * @param prevStep previous measurement.
+         * @param threshold Max time between the previous measurement and 
+         * the current one 
+         */
+        private TimeMeasurements(String script, TimeMeasurements prevStep, 
+            Double threshold)
+        {
+            this.script = script;
+            this.prevStep = prevStep;
+            this.threshold = threshold;
+        }
+        
+        /**
+         * Returns the threshold property.
+         * @return the threshold property.
+         */
+        public Double getThreshold()
+        {
+            return threshold;
+        }
+        
+        /**
+         * Returns prevStep property.
+         * @return prevStep property.
+         */
+        public TimeMeasurements getPrevStep()
+        {
+            return prevStep;
+        }
+        
+        /**
+         * Executes the script property for passed WebDriver and returns
+         * time measurement
+         * @param w participant
+         * @return time in ms for the measurement.
+         */
+        public Double execute(WebDriver w) 
+        {
+            Object res = ((JavascriptExecutor) w).executeScript(script);
+            
+            if(res instanceof Number)
+                return ((Number)res).doubleValue();
+            else if(res == null)
+                return null;
+            else 
+                fail("Wrong type returned from selenium!");
+            return null;
+        }
+        
+    };
+
+    /**
+     * If jiconop is enabled the value will be 
+     * TimeMeasurements.CONNECTION_ATTACHING otherwise the value will be
+     * TimeMeasurements.CONNECTION_CONNECTING.
+     */
+    private static TimeMeasurements connectingTime;
+
+    /**
+     * If jiconop is enabled the value will be 
+     * TimeMeasurements.CONNECTION_ATTACHED otherwise the value will be
+     * TimeMeasurements.CONNECTION_CONNECTED.
+     */
+    private static TimeMeasurements connectedTime;
+    
+    /**
+     * The time measurement type that is currently evaluated.  
+     */
+    private TimeMeasurements timeMeasurementToProcess = null;
+    
+    /**
+     * Property used to store the gathered data.
+     */
+    private static Double[][] data 
+        = new Double[TimeMeasurements.length][NUMBER_OF_CONFERENCES];
+    
+    /**
+     * Constructs test
+     * @param name the method name for the test.
+     * @param t time measurement that will be tested
+     */
+    public PerformanceTimeTest(String name, TimeMeasurements t)
+    {
+        super(name);
+        this.timeMeasurementToProcess = t;
+    }
+    
+    /**
+     * Orders the tests.
+     * @return the suite with order tests.
+     */
+    public static junit.framework.Test suite()
+    {
+        TestSuite suite = new TestSuite();
+        suite.addTest(
+            new PerformanceTimeTest("collectData", null));
+        suite.addTest(
+            new PerformanceTimeTest("checkIndexLoaded", TimeMeasurements.INDEX_LOADED));
+        suite.addTest(
+            new PerformanceTimeTest("checkDocumentReady", TimeMeasurements.DOCUMENT_READY));
+        suite.addTest(
+            new PerformanceTimeTest("checkConnecting", null));
+        suite.addTest(
+            new PerformanceTimeTest("checkConnected", null));
+        suite.addTest(
+            new PerformanceTimeTest("checkMUCJoined", TimeMeasurements.MUC_JOINED));
+        suite.addTest(
+            new PerformanceTimeTest("checkSessionInitiate", TimeMeasurements.SESSION_INITIATE));
+        suite.addTest(
+            new PerformanceTimeTest("checkIceChecking", TimeMeasurements.ICE_CHECKING));
+        suite.addTest(
+            new PerformanceTimeTest("checkIceConnected", TimeMeasurements.ICE_CONNECTED));
+        suite.addTest(
+            new PerformanceTimeTest("checkAudioRender", TimeMeasurements.AUDIO_RENDER));
+        suite.addTest(
+            new PerformanceTimeTest("checkVideoRender", TimeMeasurements.VIDEO_RENDER));
+
+        return suite;
+    }
+    
+    /**
+     * Defines how a test is going to be executed
+     */
+    public void runTest() {
+        switch(getName())
+        {
+        case "collectData":
+            collectData();
+            break;
+        case "checkConnecting":
+            checkTime(PerformanceTimeTest.connectingTime);
+            break;
+        case "checkConnected":
+            checkTime(PerformanceTimeTest.connectedTime);
+            break;
+        default:
+            checkTime(this.timeMeasurementToProcess);
+        }
+    }
+
+    /**
+     * Gets the time measurements for NUMBER_OF_CONFERENCES different 
+     * conferences and stores them in data array. Initializes
+     * connectingTime and connectedTime that are going to be used later. 
+     */
+    public static void collectData() 
+    {
+        for(int i = 0; i < NUMBER_OF_CONFERENCES; i++)
+        {
+            refreshSecondParticipant();
+            
+            waitForVideoRender();
+            
+            for(TimeMeasurements s : TimeMeasurements.values())
+            {
+                data[s.ordinal()][i] 
+                    = s.execute(participant);
+            }
+            
+        }
+        
+        if(data[TimeMeasurements.CONNECTION_ATTACHED.ordinal()][0] == null
+            && data[TimeMeasurements.CONNECTION_CONNECTED.ordinal()][0] == null)
+        {
+            // conference failed
+            fail("Conference failed!");
+            return;
+        }
+        else if(data[TimeMeasurements.CONNECTION_ATTACHED.ordinal()][0] != null)
+        {
+            connectingTime = TimeMeasurements.CONNECTION_ATTACHING;
+            connectedTime = TimeMeasurements.CONNECTION_ATTACHED;
+        }
+        else if(data[TimeMeasurements.CONNECTION_CONNECTED.ordinal()][0] != null)
+        {
+            connectingTime = TimeMeasurements.CONNECTION_CONNECTING;
+            connectedTime = TimeMeasurements.CONNECTION_CONNECTED;
+        }
+    }
+    
+    /**
+     * Refreshes the second participant.
+     */
+    private static void refreshSecondParticipant()
+    {
+        ConferenceFixture.close(participant);
+        participant = ConferenceFixture.startSecondParticipant();
+    }
+    
+    /**
+     * Waits for VIDEO_RENDER time measurement to be calculated. That way
+     * we can be sure that all other measurements are performed already.
+     */
+    private static void waitForVideoRender()
+    {
+        TestUtils.waitForCondition(participant, 10, new ExpectedCondition<Boolean>()
+        {
+
+            @Override
+            public Boolean apply(WebDriver w)
+            {
+                return TimeMeasurements.VIDEO_RENDER.execute(participant) != null;
+            }
+        });
+    }
+    
+    /**
+     * Evaluates passed time measurement value. Fails if the difference between
+     * the previous measurement and the current one is bigger than the 
+     * threshold for the current measurement. 
+     * @param s the time measurement that will be evaluated.
+     */
+    private void checkTime(TimeMeasurements s)
+    {
+        if(s == TimeMeasurements.MUC_JOINED)
+        {
+            checkThreshold(data[connectedTime.ordinal()], s);
+        }
+        else
+        {
+            Double[] prevStepData = s.getPrevStep() == null ? null : 
+                data[s.getPrevStep().ordinal()];
+            checkThreshold(prevStepData, s);
+        }
+    }
+    
+    /**
+     * Compares the threshold for the passed time measurement with the 
+     * median of the subtracted values of the data from the passed time 
+     * measurement and the passed array. 
+     * @param previousStepTimes array with times that will be subtract
+     * @param s the time measurement
+     */
+    private void checkThreshold(Double[] previousStepTimes, TimeMeasurements s)
+    {
+        Double[] difference = (previousStepTimes == null)?  data[s.ordinal()] :
+            subtractArrays(previousStepTimes, data[s.ordinal()]);
+        assertTrue(getMedian(difference) < s.getThreshold());
+    }
+    
+    /**
+     * Returns the median from passed array.
+     * @param data the array
+     * @return the median
+     */
+    private static Double getMedian(Double[] data) {
+        Arrays.sort(data);
+        return data[(int)data.length/2];
+    }
+    
+    /**
+     * Retuns array with elements constructed by subtracting element from a 
+     * from element from b with the same index.
+     * @param a
+     * @param b 
+     * @return new array
+     */
+    private static Double[] subtractArrays(Double[] a, Double[] b)
+    {
+        Double[] res = b.clone();
+        for(int i = 0; i < res.length; i++) {
+            res[i] -= a[i]; 
+        }
+        return res;
+    }
+}
