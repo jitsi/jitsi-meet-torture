742f39bb1e3b8cdbca5fce1684179d79f7263868
paweldomas <pawel.domas@jitsi.org>
2017-02-24 07:21:21 -0600

    fix(MeetUtils): invalid reference

--- src/test/java/org/jitsi/meet/test/util/MeetUtils.java
+++ src/test/java/org/jitsi/meet/test/util/MeetUtils.java
@@ -87,7 +87,7 @@ public class MeetUtils
         String portNumberStr
             = TestUtils.executeScriptAndReturnString(
                 participant,
-                "return APP.conference._room.jingleSession.peerconnection."
+                "return APP.conference._room.jvbJingleSession.peerconnection."
                     + "localDescription.sdp.split('\\r\\n')."
                     + "filter(function(line){ "
                     + "return line.indexOf('a=candidate:') !== -1 "
