ae2b2cea07ec01bfb26349f2bac90c8bb3b455b7
brian baldino <bbaldino@gmail.com>
2017-10-24 14:04:23 -0700

    add license and description to push_psnr_results.py

--- scripts/push_psnr_results.py
+++ scripts/push_psnr_results.py
@@ -1,4 +1,27 @@
 #!/usr/bin/python
+#
+# Copyright @ 2015 Atlassian Pty Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# This script takes in the path of a psnr output file (which should contain only
+# a float psnr value, and nothing else) as well as the url of a dashboard service
+# to which the psnr, as well as some variables from a jenkins build environment,
+# will be pushed.  Note that this does assume it's being executed as part of a
+# jenkins build, so if run elsewhere the caller should make sure those variables
+# are defined.
+# Usage example:
+# ./push_psnr_results.py ./psnr.out http://my.dashboard.service/psnrPushEndpoint
 
 import os
 import requests
