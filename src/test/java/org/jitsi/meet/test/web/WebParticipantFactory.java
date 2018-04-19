6304dfaf25a4b8ad21e1b70b3f949ae934ded7a6
damencho <damencho@jitsi.org>
2018-04-19 14:42:41 -0500

    Adds chrome argument to disable autoplay restrictions in chrome v66.

--- src/test/java/org/jitsi/meet/test/web/WebParticipantFactory.java
+++ src/test/java/org/jitsi/meet/test/web/WebParticipantFactory.java
@@ -214,6 +214,9 @@ public class WebParticipantFactory
             ops.addArguments("disable-plugins");
             ops.addArguments("mute-audio");
             ops.addArguments("disable-infobars");
+            // Since chrome v66 there are new autoplay policies, which broke
+            // shared video tests, disable no-user-gesture to make it work
+            ops.addArguments("autoplay-policy=no-user-gesture-required");
 
             if (options.getChromeExtensionId() != null)
             {
