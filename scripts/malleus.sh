04f71b4bdabb8df01e7a5253187cf3b0e20a8f84
paweldomas <pawel.domas@jitsi.org>
2018-07-19 10:57:34 -0500

    fix(scripts/malleus.sh): update and simplify
    The isRemote can be set on global "jitsi-meet" scope to apply to all
    participants.
    
    Updates property names.

--- scripts/malleus.sh
+++ scripts/malleus.sh
@@ -4,36 +4,20 @@
 #  $1 conferences, with
 #  $2 participants each, with
 #  $3 of them sending video and the rest having video muted.
+#  $4 seconds the participants will stay in the conference.
+#  $5 will be the conference room name prefix.
 
 mvn \
 -Dthreadcount=1 \
--Dmulti.conf.timeout=600 \
--Dmulti.conf.num.conferences=$1 \
--Dmulti.conf.num.participants=$2 \
--Dmulti.conf.num.senders=$3 \
+-Dorg.jitsi.malleus.conferences=$1 \
+-Dorg.jitsi.malleus.participants=$2 \
+-Dorg.jitsi.malleus.senders=$3 \
+-Dorg.jitsi.malleus.duration=$4 \
+-Dorg.jitsi.malleus.room_name_prefix=$5 \
 -Djitsi-meet.tests.toRun=MalleusJitsificus \
 -Dremote.address=http://grid.example.com:4444/wd/hub \
 -Dwdm.gitHubTokenName=jitsi-jenkins \
 -Dremote.resource.path=/usr/share/jitsi-meet-torture \
--Djitsi-meet.instance.url=https://beta.meet.jit.si \
--Dweb.participant1.isRemote=true \
--Dweb.participant2.isRemote=true \
--Dweb.participant3.isRemote=true \
--Dweb.participant4.isRemote=true \
--Dweb.participant5.isRemote=true \
--Dweb.participant6.isRemote=true \
--Dweb.participant7.isRemote=true \
--Dweb.participant8.isRemote=true \
--Dweb.participant9.isRemote=true \
--Dweb.participant10.isRemote=true \
--Dweb.participant11.isRemote=true \
--Dweb.participant12.isRemote=true \
--Dweb.participant13.isRemote=true \
--Dweb.participant14.isRemote=true \
--Dweb.participant15.isRemote=true \
--Dweb.participant16.isRemote=true \
--Dweb.participant17.isRemote=true \
--Dweb.participant18.isRemote=true \
--Dweb.participant19.isRemote=true \
--Dweb.participant20.isRemote=true \
+-Djitsi-meet.instance.url=https://meet.jit.si \
+-Djitsi-meet.isRemote=true \
 test
