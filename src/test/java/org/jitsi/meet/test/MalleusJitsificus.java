19305a5c747024451750af4d496393ed8c25c912
George Politis <gp@jitsi.org>
2019-10-28 17:22:09 +0100

    Parallel participant closing.

--- src/test/java/org/jitsi/meet/test/MalleusJitsificus.java
+++ src/test/java/org/jitsi/meet/test/MalleusJitsificus.java
@@ -165,6 +165,7 @@ public class MalleusJitsificus
             finally
             {
                 participant.hangUp();
+                closeParticipant(participant);
             }
         });
 
