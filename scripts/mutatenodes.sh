2513cccaab23697086a6288770dfd3a0e8fe7454
George Politis <gp@jitsi.org>
2019-12-02 16:20:15 +0000

    it's maxSession not maxSessions

--- scripts/mutatenodes.sh
+++ scripts/mutatenodes.sh
@@ -36,17 +36,17 @@ mutate_node() {
   then
     # mutate the node into a sender.
     max_instances=1
-    max_sessions=1
+    max_session=1
     application_name='ljmSender'
     NUM_SENDERS=$(echo "$NUM_SENDERS-1" | bc)
   else
     # mutate the node into a receiver.
     max_instances=20
-    max_sessions=20
+    max_session=20
     application_name='ljmReceiver'
   fi
   $SSH $node cat /opt/selenium_grid_extras/node_5555.json \
-      | jq ".capabilities[0].maxInstances = $max_instances | .capabilities[0].applicationName = \"$application_name\" | .maxSessions = $max_sessions" > $node_config
+      | jq ".capabilities[0].maxInstances = $max_instances | .capabilities[0].applicationName = \"$application_name\" | .maxSession = $max_session" > $node_config
 
   $SCP $node_config $node:node_5555.json
   $SSH $node "sudo mv node_5555.json /opt/selenium_grid_extras/; sudo chown selenium:selenium /opt/selenium_grid_extras/node_5555.json; sudo chmod 644 /opt/selenium_grid_extras/node_5555.json; sudo systemctl restart selenium-grid-extras-node"
