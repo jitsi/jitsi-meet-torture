398a42d4adf8455e36ab1aa7267c3a5de5c03c9f
damencho <damencho@jitsi.org>
2020-12-04 12:48:10 -0600

    feat: Store the URL with all options when using it.

--- src/test/java/org/jitsi/meet/test/base/Participant.java
+++ src/test/java/org/jitsi/meet/test/base/Participant.java
@@ -73,6 +73,11 @@ public abstract class Participant<T extends WebDriver>
     private String joinedRoomName = null;
 
     /**
+     * The url used to join.
+     */
+    private JitsiMeetUrl meetUrl = null;
+
+    /**
      * Executor that is responsible for keeping the participant session alive.
      */
     private ScheduledExecutorService executor
@@ -152,6 +157,7 @@ public abstract class Participant<T extends WebDriver>
         doJoinConference(meetURL);
 
         this.joinedRoomName = meetURL.getRoomName();
+        this.meetUrl = meetURL;
         this.hungUp = false;
     }
 
@@ -597,5 +603,14 @@ public abstract class Participant<T extends WebDriver>
             .filter(p -> p.getEndpointId().equals(id))
             .findFirst()
             .orElse(null);
+    }
+
+    /**
+     * The url with all options used to join the conference.
+     * @return
+     */
+    public JitsiMeetUrl getMeetUrl()
+    {
+        return meetUrl;
     }
 }
