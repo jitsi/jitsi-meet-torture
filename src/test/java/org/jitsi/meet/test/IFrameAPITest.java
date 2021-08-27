e15a68c145d362ac3f817a039feef30c76844013
Дамян Минков <damencho@jitsi.org>
2021-08-27 11:35:42 -0500

    feat: Adds iframe getParticipantsInfo test.
    https://jitsi.github.io/handbook/docs/dev-guide/dev-guide-iframe#getparticipantsinfo

--- src/test/java/org/jitsi/meet/test/IFrameAPITest.java
+++ src/test/java/org/jitsi/meet/test/IFrameAPITest.java
@@ -17,12 +17,18 @@ package org.jitsi.meet.test;
 
 import com.google.gson.*;
 import org.jitsi.meet.test.base.*;
+import org.jitsi.meet.test.pageobjects.web.*;
+import org.jitsi.meet.test.util.*;
 import org.jitsi.meet.test.web.*;
 
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.*;
 import org.testng.*;
 import org.testng.annotations.*;
 
 import java.net.*;
+
+import static org.testng.Assert.*;
 
 /**
  * Test that loads a page using the iframe API to load a meeting.
@@ -49,10 +55,13 @@ public class IFrameAPITest
     private static final String IFRAME_ROOM_PARAMS
         = "domain=%s&room=%s"
         // here goes the default settings, used in Participant join function
-        + "&config=%s&interfaceConfig=%s";
+        + "&config=%s&interfaceConfig=%s&userInfo=%s";
 
-    @Test
-    public void testIFrameAPI()
+    /**
+     * Constructs an JitsiMeetUrl to be used with iframeAPI.
+     * @return url that will load a meeting in an iframe.
+     */
+    private JitsiMeetUrl getIFrameUrl(JsonObject userInfo)
     {
         String pagePath = System.getProperty(IFRAME_PAGE_PATH_PNAME);
 
@@ -80,13 +89,25 @@ public class IFrameAPITest
             domain,
             currentRoomName,
             defaultParams.get("config").toString(),
-            defaultParams.get("interfaceConfig").toString());
+            defaultParams.get("interfaceConfig").toString(),
+            userInfo != null ? userInfo : "");
 
         // Override the server and the path part(which is s room name)
         iFrameUrl.setServerUrl(pagePath);
         iFrameUrl.setRoomName(IFRAME_ROOM_NAME);
         iFrameUrl.setRoomParameters(roomParams);
         iFrameUrl.setIframeToNavigateTo("jitsiConferenceFrame0");
+
+        return iFrameUrl;
+    }
+
+    /**
+     * Opens the iframe and executes SwitchVideoTest and MuteTest.
+     */
+    @Test
+    public void testIFrameAPI()
+    {
+        JitsiMeetUrl iFrameUrl = getIFrameUrl(null);
 
         ensureOneParticipant(iFrameUrl);
         ensureThreeParticipants(iFrameUrl, null, null);
@@ -103,5 +124,61 @@ public class IFrameAPITest
         muteTest.muteParticipant1AndCheck();
         muteTest.muteParticipant2AndCheck();
         muteTest.muteParticipant3AndCheck(iFrameUrl, null, null);
+
+        hangUpAllParticipants();
+    }
+
+    /**
+     * Functions testing:
+     * https://jitsi.github.io/handbook/docs/dev-guide/dev-guide-iframe#getparticipantsinfo
+     *
+     * Tests and passing userInfo to JitsiMeetExternalAPI options
+     */
+    @Test(dependsOnMethods = { "testIFrameAPI" })
+    public void testGetParticipantsInfo()
+    {
+        String displayName = "John Doe";
+        String email = "email@jitsiexamplemail.com";
+
+        JsonObject userInfo = new JsonObject();
+        userInfo.addProperty("email", email);
+        userInfo.addProperty("displayName", displayName);
+
+        JitsiMeetUrl iFrameUrl = getIFrameUrl(userInfo);
+
+        ensureOneParticipant(iFrameUrl);
+
+        WebParticipant participant1 = getParticipant1();
+        WebDriver driver = participant1.getDriver();
+
+        driver.switchTo().defaultContent();
+        String s = TestUtils.executeScriptAndReturnString(driver,
+            "return JSON.stringify(window.jitsiAPI.getParticipantsInfo());");
+
+        // returns an array of participants, in this case just us
+        JsonObject participantsInfo
+            = new JsonParser().parse(s).getAsJsonArray().get(0).getAsJsonObject();
+
+        // check displayName from getParticipantsInfo, returned as "John Doe (me)"
+        assertTrue(
+            participantsInfo.get("formattedDisplayName").getAsString().contains(displayName),
+            "Wrong display name from iframeAPI.getParticipantsInfo");
+
+        if (iFrameUrl.getIframeToNavigateTo() != null)
+        {
+            // let's wait for switch to that iframe so we can continue with regular tests
+            WebDriverWait wait = new WebDriverWait(driver, 10);
+            wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(
+                By.id(iFrameUrl.getIframeToNavigateTo())));
+        }
+
+        DisplayNameTest.checkDisplayNameOnLocalVideo(participant1, displayName);
+
+        // check displayName and email in settings through UI
+        participant1.getToolbar().clickSettingsButton();
+        SettingsDialog settingsDialog = participant1.getSettingsDialog();
+        settingsDialog.waitForDisplay();
+        assertEquals(settingsDialog.getDisplayName(), displayName);
+        assertEquals(settingsDialog.getEmail(), email);
     }
 }
