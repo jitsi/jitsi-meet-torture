9de0fa262e656a1ca0b923ff2bd264d458260bbd
Дамян Минков <damencho@jitsi.org>
2021-09-23 08:44:44 -0500

    fix: Fixes raise hand test and NPE.

--- src/test/java/org/jitsi/meet/test/IFrameAPITest.java
+++ src/test/java/org/jitsi/meet/test/IFrameAPITest.java
@@ -1956,8 +1956,7 @@ public class IFrameAPITest
         mainButtons.put("desktop", Toolbar.DESKTOP);
         mainButtons.put("microphone", Toolbar.AUDIO_MUTE);
 
-        mainButtons.put("raisehand", participant1.getToolbar().areReactionsEnabled()
-            ? Toolbar.REACTIONS_MENU : Toolbar.RAISE_HAND);
+        mainButtons.put("raisehand", Toolbar.RAISE_HAND);
         mainButtons.put("participants-pane", Toolbar.PARTICIPANTS);
         mainButtons.put("tileview", Toolbar.TILE_VIEW_BUTTON);
         mainButtons.put("hangup", Toolbar.HANGUP);
