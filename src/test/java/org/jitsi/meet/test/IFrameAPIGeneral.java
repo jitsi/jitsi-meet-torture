9953d0aad5e13d8128342607a9e50b3609515125
Дамян Минков <damencho@jitsi.org>
2022-02-24 11:51:55 -0600

    fix: Fixes custom buttons IFrameAPIGeneral test for non moderators.

--- src/test/java/org/jitsi/meet/test/IFrameAPIGeneral.java
+++ src/test/java/org/jitsi/meet/test/IFrameAPIGeneral.java
@@ -129,8 +129,14 @@ public class IFrameAPIGeneral
         }
 
         mainButtons.put("invite", Toolbar.INVITE);
-        mainButtons.put("mute-everyone", Toolbar.MUTE_EVERYONE_AUDIO);
-        mainButtons.put("mute-video-everyone", Toolbar.MUTE_EVERYONE_VIDEO);
+
+        // these buttons are available only for moderators
+        if (participant1.isModerator())
+        {
+            mainButtons.put("mute-everyone", Toolbar.MUTE_EVERYONE_AUDIO);
+            mainButtons.put("mute-video-everyone", Toolbar.MUTE_EVERYONE_VIDEO);
+        }
+
         mainButtons.put("profile", Toolbar.PROFILE);
         mainButtons.put("security", Toolbar.SECURITY);
         mainButtons.put("select-background", Toolbar.SELECT_BACKGROUND);
