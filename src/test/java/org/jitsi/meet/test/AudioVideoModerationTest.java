08f2bab33418ca87224802f806e6ef39e1b80bdb
Boris Grozev <boris@jitsi.org>
2022-04-20 09:05:24 -0500

    ref: Remove code duplication.

--- src/test/java/org/jitsi/meet/test/AudioVideoModerationTest.java
+++ src/test/java/org/jitsi/meet/test/AudioVideoModerationTest.java
@@ -179,48 +179,21 @@ public class AudioVideoModerationTest
         participant3.getToolbar().clickRaiseHandButton();
 
         participant1.hangUp();
-
         joinFirstParticipant();
 
-        if (participant2.isModerator())
-        {
-            participant2.getParticipantsPane().open();
+        // After p1 re-joins either p2 or p3 is promoted to moderator.
+        WebParticipant moderator = participant2.isModerator() ? participant2 : participant3;
+        WebParticipant nonModerator = participant2.isModerator() ? participant3 : participant2;
 
-            participant2.getParticipantsPane().askToUnmute(participant3);
-
-            participant3.getNotifications().getAskToUnmuteNotification();
-
-            MeetUIUtils.toggleAudioAndCheck(participant3, participant1, false);
-            MeetUIUtils.unmuteVideoAndCheck(participant3, participant1);
-
-            participant2.getParticipantsPane().clickContextMenuButton();
-
-            participant2.getAVModerationMenu().clickStopAudioModeration();
-
-            participant2.getAVModerationMenu().clickStopVideoModeration();
-
-            participant2.getParticipantsPane().close();
-        }
-
-        if (participant3.isModerator())
-        {
-            participant3.getParticipantsPane().open();
-
-            participant3.getParticipantsPane().askToUnmute(participant2);
-
-            participant2.getNotifications().getAskToUnmuteNotification();
-
-            MeetUIUtils.toggleAudioAndCheck(participant2, participant1, false);
-            MeetUIUtils.unmuteVideoAndCheck(participant2, participant1);
-
-            participant3.getParticipantsPane().clickContextMenuButton();
-
-            participant3.getAVModerationMenu().clickStopAudioModeration();
-
-            participant3.getAVModerationMenu().clickStopVideoModeration();
-
-            participant3.getParticipantsPane().close();
-        }
+        moderator.getParticipantsPane().open();
+        moderator.getParticipantsPane().askToUnmute(nonModerator);
+        nonModerator.getNotifications().getAskToUnmuteNotification();
+        MeetUIUtils.toggleAudioAndCheck(nonModerator, participant1, false);
+        MeetUIUtils.unmuteVideoAndCheck(nonModerator, participant1);
+        moderator.getParticipantsPane().clickContextMenuButton();
+        moderator.getAVModerationMenu().clickStopAudioModeration();
+        moderator.getAVModerationMenu().clickStopVideoModeration();
+        moderator.getParticipantsPane().close();
     }
 
     /**
