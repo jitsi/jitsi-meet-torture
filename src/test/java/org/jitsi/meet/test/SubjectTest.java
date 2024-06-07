e84fddd2e994a40ab20c1111f951879ebbc1c279
damencho <damencho@jitsi.org>
2024-06-07 11:23:00 -0500

    squash: Missing license and comments.

--- src/test/java/org/jitsi/meet/test/SubjectTest.java
+++ src/test/java/org/jitsi/meet/test/SubjectTest.java
@@ -1,6 +1,20 @@
+/*
+ * Copyright @ 2015 Atlassian Pty Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.jitsi.meet.test;
 
-import org.jitsi.meet.test.util.*;
 import org.jitsi.meet.test.web.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.interactions.*;
@@ -9,9 +23,13 @@ import org.testng.annotations.*;
 
 import java.util.logging.*;
 
-import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
+/**
+ * TSets subject via config url hash param and check locally and remote for it.
+ *
+ * @author Damian Minkov
+ */
 public class SubjectTest
     extends WebTestBase
 {
