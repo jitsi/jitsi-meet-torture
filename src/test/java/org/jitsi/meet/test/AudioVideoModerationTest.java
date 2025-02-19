6a0c803259e9054a9512770f6159179f5f37ffe2
damencho <damencho@jitsi.org>
2025-02-19 07:48:24 -0600

    fix: Adopts to UI changes.

--- src/test/java/org/jitsi/meet/test/AudioVideoModerationTest.java
+++ src/test/java/org/jitsi/meet/test/AudioVideoModerationTest.java
@@ -204,7 +204,7 @@ public class AudioVideoModerationTest
         Actions moderatorActions = new Actions(moderatorDriver);
 
         moderator.getParticipantsPane().open();
-        moderator.getParticipantsPane().allowVideo(nonModerator, false);
+        moderator.getParticipantsPane().allowVideo(nonModerator, true);
         moderator.getParticipantsPane().askToUnmute(nonModerator, false);
         nonModerator.getNotifications().getAskToUnmuteNotification();
         MeetUIUtils.unmuteAudioAndCheck(nonModerator, participant1);
@@ -404,6 +404,8 @@ public class AudioVideoModerationTest
 
         askParticipantToUnmute(moderator, participant);
 
+        participant.getNotifications().closeAskToUnmuteNotification();
+
         MeetUIUtils.unmuteAudioAndCheck(participant, moderator);
         MeetUIUtils.unmuteVideoAndCheck(participant, moderator);
 
@@ -437,7 +439,7 @@ public class AudioVideoModerationTest
     {
         ParticipantsPane participantsPane = moderator.getParticipantsPane();
 
-        participantsPane.allowVideo(participant, false);
+        participantsPane.allowVideo(participant, true);
         participantsPane.askToUnmute(participant, false);
 
         participant.getNotifications().getAskToUnmuteNotification();
